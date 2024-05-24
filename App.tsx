47f7e356f03ba038ca4be3840c0cf1148adbc0c3
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-24 18:25:54 -0300

    feature: Loading component started

--- App.tsx
+++ App.tsx
@@ -6,12 +6,15 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
   });
+
+  if (!fontsLoaded) return <Loading />;
 
   return (
     <GluestackUIProvider>
