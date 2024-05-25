dbc764747249e98f705fec71a871fa71214b7d13
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-25 16:50:25 -0300

    fix: fixing structure

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,14 @@
-import { Center, Image, VStack, Text } from '@gluestack-ui/themed';
+import {
+  Center,
+  Image,
+  VStack,
+  Text,
+  Heading,
+  Box,
+} from '@gluestack-ui/themed';
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
+import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
@@ -13,12 +21,23 @@ export function SignIn() {
         position='absolute'
       />
 
-      <Center my='$24'>
+      <Center my='$24' gap='$1.5'>
         <LogoSvg />
 
         <Text color='$gray100' fontSize='$sm'>
           Treine sua mente e o seu corpo.
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color='$gray100' fontSize='$xl' fontFamily='$heading'>
+          Acesse sua conta
+        </Heading>
+
+        <Center gap='$2' px='$5' my='$6'>
+          <Input placeholder='E-mail' />
+          <Input placeholder='Senha' />
+        </Center>
       </Center>
     </VStack>
   );
