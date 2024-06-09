e32aa3aa196c828829a9b71ea34582773ae9639e
Antonio Vuono <antoniosvuono@icloud.com>
2024-06-09 12:43:00 -0300

    feature: add section list with  history card

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,46 @@
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { SectionList, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { useAppTheme } from 'src/theme';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal'],
+    },
+  ]);
+
+  const { colors, fontSizes, fonts } = useAppTheme();
+
   return (
     <>
       <ScreenHeader title='Histórico de Exercícios' />
       <View style={{ flex: 1, padding: 14 }}>
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
+        <SectionList
+          sections={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <HistoryCard />}
+          renderSectionHeader={({ section }) => (
+            <Text
+              style={{
+                color: colors.gray200,
+                fontSize: fontSizes.md,
+                fontFamily: fonts.heading.fontFamily,
+                paddingTop: 40,
+                paddingBottom: 12,
+              }}
+            >
+              {section.title}
+            </Text>
+          )}
+        />
       </View>
     </>
   );
