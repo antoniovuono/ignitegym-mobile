73a50b9067a100f6bd00a329fc4ae657d96276a0
Antonio Vuono <antoniosvuono@icloud.com>
2024-06-11 22:23:36 -0300

    feature: exercise finalized

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,13 @@
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useAppTheme } from 'src/theme';
 import { useNavigation } from '@react-navigation/native';
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import BodySvg from '@assets/body.svg';
-import { Text } from 'react-native-paper';
+import { Card, Text } from 'react-native-paper';
+import SeriesSvg from '@assets/series.svg';
+import RepetitionsSvg from '@assets/repetitions.svg';
+import { Button } from '@components/Button';
 
 export function Exercise() {
   const { colors, fontSizes, fonts } = useAppTheme();
@@ -64,6 +67,71 @@ export function Exercise() {
           >
             Costas
           </Text>
+        </View>
+      </View>
+
+      <View style={{ padding: 14 }}>
+        <Image
+          source={{
+            uri: 'https://uploads.metropoles.com/wp-content/uploads/2023/02/17143449/GettyImages-946365998-1.jpg',
+          }}
+          height={384}
+          borderRadius={6}
+          style={{
+            marginBottom: 16,
+          }}
+        />
+
+        <View>
+          <Card
+            contentStyle={{
+              backgroundColor: colors.gray600,
+              borderRadius: 8,
+              padding: 16,
+            }}
+          >
+            <Card.Content
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginBottom: 24,
+              }}
+            >
+              <Card.Content
+                style={{ flexDirection: 'row', alignItems: 'center' }}
+              >
+                <SeriesSvg />
+                <Text
+                  style={{
+                    color: colors.gray100,
+                    fontFamily: fonts.body.fontFamily,
+                    fontSize: fontSizes.md,
+                    marginLeft: 8,
+                  }}
+                >
+                  3 séries
+                </Text>
+              </Card.Content>
+
+              <Card.Content
+                style={{ flexDirection: 'row', alignItems: 'center' }}
+              >
+                <RepetitionsSvg />
+                <Text
+                  style={{
+                    color: colors.gray100,
+                    fontFamily: fonts.body.fontFamily,
+                    fontSize: fontSizes.md,
+                    marginLeft: 8,
+                  }}
+                >
+                  12 repetições
+                </Text>
+              </Card.Content>
+            </Card.Content>
+
+            <Button title='Marcar como realizado' isLoading={false} />
+          </Card>
         </View>
       </View>
     </View>
