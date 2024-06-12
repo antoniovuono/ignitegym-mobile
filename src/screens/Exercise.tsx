1e972737e1004b8d1e2560602335eb81724d1c17
Antonio Vuono <antoniosvuono@icloud.com>
2024-06-11 22:11:51 -0300

    feature: exercises header finalized

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,71 @@
-import { Center } from '@components/Center';
+import { TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useAppTheme } from 'src/theme';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import BodySvg from '@assets/body.svg';
 import { Text } from 'react-native-paper';
 
 export function Exercise() {
+  const { colors, fontSizes, fonts } = useAppTheme();
+
+  const { goBack } = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text style={{ color: 'white' }}>Exercises</Text>
-    </Center>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: colors.gray600,
+          paddingTop: 52,
+          paddingHorizontal: 32,
+          paddingBottom: 32,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <View>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Feather name='arrow-left' size={24} color={colors.green500} />
+          </TouchableOpacity>
+          <Text
+            style={{
+              color: colors.gray100,
+              fontSize: fontSizes.lg,
+              fontFamily: fonts.heading.fontFamily,
+              marginTop: 10,
+              flexShrink: 1,
+            }}
+          >
+            Puxada frontal
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 10,
+          }}
+        >
+          <BodySvg />
+          <Text
+            style={{
+              color: colors.gray100,
+              fontSize: fontSizes.sm,
+              fontFamily: fonts.body.fontFamily,
+              marginLeft: 4,
+            }}
+          >
+            Costas
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 }
